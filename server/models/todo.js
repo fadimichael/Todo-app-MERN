c00a2533050cb37dda92d1b3114da64c5b5f044c
fadimichael <fadi.michael@gmx.de>
2023-05-01 16:51:03 +0200

    chor: versel delploy

--- server/models/todo.js
+++ server/models/todo.js
@@ -20,9 +20,12 @@ const TodoSchema = new Schema({
   // },
 });
 
-const Todo = mongoose.model("Todo", TodoSchema);
+const Todo = mongoose.model("Todo", TodoSchema, "FADI");
 
-// //Todo is a name we gave to our Schema
+// //Todo is a name we gave to our Schema it will build a new collection in mongodb with the name
+// todos that mean (klein geschrieben und extra (s) am ende)
 //TodoSchema is our actual Schema name
 
+//if you want your own name and capital or small then you have to wirte it in the third Filed like FADI
+// it will be FADI capital and without S and the end;
 module.exports = Todo;
--- server/utils/connectDB.js
+++ server/utils/connectDB.js
@@ -3,15 +3,17 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const colors = require("colors");
 dotenv.config({ path: "../config.env" });
-// dotenv.config({ path: "../config.env" });
-const url = process.env.MONGO_DB_ATLAS_URI;
+
+const URL = process.env.MONGO_DB_ATLAS_URI;
+const DbName = process.env.MONGO_DB_NAME;
 
 const connectDB = () => {
   mongoose.set("strictQuery", false);
   try {
-    mongoose.connect(url, {
+    mongoose.connect(URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: DbName,
     });
     console.log(colors.bgYellow("MongoDB connected ..."));
   } catch (err) {
